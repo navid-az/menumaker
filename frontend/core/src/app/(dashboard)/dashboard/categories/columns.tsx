ad3ec18a3a8def85cc9357e1c8a29496a0b340f0
Navid Azimi <navidazimi1380@gmail.com>
2024-06-23 16:05:49 +0330

    feat(frontend): :sparkles: newcategory delete button
    * refactor:  actions.tsx functions

--- frontend/core/src/app/(dashboard)/dashboard/categories/columns.tsx
+++ frontend/core/src/app/(dashboard)/dashboard/categories/columns.tsx
@@ -6,16 +6,36 @@ import { ColumnDef } from "@tanstack/react-table";
 //components
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+//actions
+import { deleteCategory } from "@/app/actions";
 
 //SVGs
-import { ArrowUpDown, ImageIcon } from "lucide-react";
+import { ArrowUpDown, ImageIcon, MoreVertical, Trash2 } from "lucide-react";
 
 //types
+import { CellContext } from "@tanstack/react-table";
 export type Category = {
   id: number;
+  menu: string;
   name: string;
   icon: { name: string; image: string };
   is_active: boolean;
+};
+
+const handleDelete = async (props: CellContext<Category, unknown>) => {
+  deleteCategory(props.row.original.menu, props.row.original.id);
 };
 
 export const categoryColumns: ColumnDef<Category>[] = [
@@ -64,6 +84,42 @@ export const categoryColumns: ColumnDef<Category>[] = [
         checked={props.cell.getValue() as boolean}
         id={props.row.id}
       ></Switch>
+    ),
+  },
+  {
+    id: "row-options",
+    cell: (props) => (
+      <div className="flex items-center gap-2">
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Trash2 className="h-5 w-5"></Trash2>
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>مطمعنی؟</AlertDialogTitle>
+              <AlertDialogDescription>
+                آیا از حذف این دسته بندی({props.row.getValue("name")}) مطمعنی؟
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>انصراف</AlertDialogCancel>
+              <AlertDialogAction asChild>
+                <Button
+                  onClick={() => handleDelete(props)}
+                  variant="destructive"
+                >
+                  حذف کن
+                </Button>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <Button variant="ghost" size="icon">
+          <MoreVertical className="h-5 w-5"></MoreVertical>
+        </Button>
+      </div>
     ),
   },
 ];
--- frontend/core/src/app/actions.tsx
+++ frontend/core/src/app/actions.tsx
@@ -1,16 +1,17 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import jwtDecoder from "@/lib/jwtDecoder";
 import { cookies } from "next/headers";
 import { revalidateTag } from "next/cache";
-type DecodedJwtType = {
-  token_type: "access" | "refresh";
-  exp: number;
-  iat: number;
-  jti: string;
-  user_id: number;
-};
+
+// import jwtDecoder from "@/lib/jwtDecoder";
+// type DecodedJwtType = {
+//   token_type: "access" | "refresh";
+//   exp: number;
+//   iat: number;
+//   jti: string;
+//   user_id: number;
+// };
 
 export async function createCookie(
   name: string,
@@ -21,32 +22,10 @@ export async function createCookie(
   redirect("/");
 }
 
-export async function getItems() {
-  const res = await fetch("http://127.0.0.1:8000/dashboard/items/");
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
-
 type ItemType = {
   name: string;
   category: number;
 };
-export async function createItem(data: ItemType) {
-  const res = await fetch("http://127.0.0.1:8000/menu/venhan/items/create/", {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  console.log("HEADERS:", res.headers);
-  const items = await res.json();
-
-  return items;
-}
 
 //logOut user
 export async function logOut() {
@@ -75,6 +54,52 @@ export async function verifyToken() {
 }
 
 //~~~~dashboard table mutation-related actions~~~
+
+//CATEGORY ACTIONS
+export async function deleteCategory(menuSlug: string, categoryId: number) {
+  const accessToken = cookies().get("access");
+
+  const res = await fetch(
+    `http://127.0.0.1:8000/menu/${menuSlug}/categories/${categoryId}/delete/`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken?.value}`,
+      },
+    }
+  );
+  if (!res.ok) {
+    return new Error("wtf is this shit bro?");
+  }
+  revalidateTag("categories");
+}
+
+//ITEM ACTIONS
+//get all menu items
+export async function getItems() {
+  const res = await fetch("http://127.0.0.1:8000/dashboard/items/");
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export async function createItem(data: ItemType) {
+  const res = await fetch("http://127.0.0.1:8000/menu/venhan/items/create/", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  console.log("HEADERS:", res.headers);
+  const items = await res.json();
+
+  return items;
+}
+
 export async function updateItem(
   menuSlug: string,
   itemId: number,
