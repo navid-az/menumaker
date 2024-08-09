fa59e3bd3b19aa2c757bece68936c352bdecccc4
Navid Azimi <navidazimi1380@gmail.com>
2024-08-09 12:34:40 +0330

    fix(frontend): :bug: improve sliderTab click event

--- frontend/core/src/app/(creator)/builder/components/SliderTab.tsx
+++ frontend/core/src/app/(creator)/builder/components/SliderTab.tsx
@@ -18,54 +18,64 @@ type SliderTabType = {
 
 type SliderTabTitle = {
   title: string;
+  description?: string;
   iconSrc: string;
   children: React.ReactNode;
 };
 
 type SliderTabBody = {
   isOpen?: boolean;
-  description?: string;
   children?: React.ReactNode;
 };
 
 export function SliderTab({ isActive, onClick, children }: SliderTabType) {
   return (
-    <Label
+    <div
       onClick={onClick}
       className={cn(
-        `flex w-full cursor-pointer select-none flex-col justify-between gap-2 rounded-lg border-[3px] bg-soft-blue p-2 text-royal-green transition-all duration-200 ease-in-out sm:p-3 ${
+        `flex w-full cursor-pointer select-none flex-col justify-between rounded-lg border-[3px] bg-soft-blue text-royal-green transition-all duration-200 ease-in-out ${
           isActive ? "border-royal-green" : "border-sad-blue"
         }`
       )}
     >
       {children}
-    </Label>
-  );
-}
-
-export function SliderTabTitle({ title, iconSrc, children }: SliderTabTitle) {
-  return (
-    <div className="flex w-full cursor-pointer items-center justify-between">
-      <div className="flex items-center justify-between gap-2">
-        <div className="relative aspect-square w-6 sm:w-7">
-          <Image src={iconSrc} fill={true} alt="card-icon"></Image>
-        </div>
-        <h3 className="text-lg font-bold sm:text-2xl">{title}</h3>
-      </div>
-      {children}
     </div>
   );
 }
 
-export function SliderTabBody({
-  isOpen,
+export function SliderTabTitle({
+  title,
   description,
+  iconSrc,
   children,
-}: SliderTabBody) {
+}: SliderTabTitle) {
   return (
-    <div className="flex flex-col gap-2">
+    <Label className="flex w-full cursor-pointer flex-col justify-between p-2 sm:p-3">
+      <div className="flex justify-between">
+        <div className="flex items-center justify-between gap-2">
+          <div className="relative aspect-square w-6 sm:w-7">
+            <Image src={iconSrc} fill={true} alt="card-icon"></Image>
+          </div>
+          <h3 className="text-lg font-bold sm:text-2xl">{title}</h3>
+        </div>
+        {children}
+      </div>
       <p className="text-sm font-normal sm:text-base">{description}</p>
-      <div className={`${isOpen ? "flex" : "hidden"}`}>{children}</div>
+    </Label>
+  );
+}
+
+export function SliderTabBody({ isOpen, children }: SliderTabBody) {
+  const handleBodyClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div
+      onClick={handleBodyClick}
+      className={`${isOpen ? "flex" : "hidden"} p-2 pt-0 sm:p-3 sm:pt-0`}
+    >
+      {children}
     </div>
   );
 }
