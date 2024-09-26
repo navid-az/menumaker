b38220870d7fda0a64de92fd6e6a6a40999b387e
Navid Azimi <navidazimi1380@gmail.com>
2024-09-26 12:41:08 +0330

    refactor(frontend): :lipstick: implement minor sizing support (subject to refinement)
    + the sizing support will be improved soon

--- frontend/core/src/app/(menu)/[menu_id]/menu/components/PriceTag.tsx
+++ frontend/core/src/app/(menu)/[menu_id]/menu/components/PriceTag.tsx
@@ -4,6 +4,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 //utilities
 import { formatCurrency } from "../../utilities/formatCurrency";
 
+//libraries
+import { cva } from "class-variance-authority";
+
 //SVGs
 import { Toman } from "../../components";
 import { useEffect, useState } from "react";
@@ -12,6 +15,7 @@ import { useEffect, useState } from "react";
 type PriceTagType = {
   price: number;
   unitDisplayType?: "default" | "minimal" | "compact" | "engMinimal";
+  size?: "default" | "sm" | "lg";
   removeZeroes?: boolean;
   isLoading?: boolean;
   isFree?: boolean;
@@ -20,12 +24,12 @@ type PriceTagType = {
 export default function PriceTag({
   price = 0,
   unitDisplayType,
+  size,
   removeZeroes = false,
   isLoading,
   isFree = false,
 }: PriceTagType) {
   const secondaryColor = "#FFFF";
-  const number = formatCurrency(price);
   const [validatedPrice, setValidatedPrice] = useState<string | number>(price);
 
   useEffect(() => {
@@ -57,17 +61,16 @@ export default function PriceTag({
 
   return (
     <span className={`flex w-max items-center justify-between gap-1`}>
-      {isLoading ? (
-        <div className={`text-${secondaryColor}`}>
-          <div className="flex h-6 w-20 gap-1">
-            <Skeleton className="h-full w-8/12" />
-            <Skeleton className="h-full w-4/12" />
-          </div>
-        </div>
-      ) : (
+      {!isLoading ? (
         <div className="flex items-center gap-1">
           <p
-            className={`text-${secondaryColor} text-base font-semibold sm:text-2xl`}
+            className={`text-${secondaryColor} mt-1 font-semibold ${
+              size === "sm"
+                ? "text-xl"
+                : size === "lg"
+                ? "text-3xl"
+                : "text-2xl"
+            } sm:text-2xl`}
           >
             {validatedPrice}
           </p>
@@ -78,7 +81,15 @@ export default function PriceTag({
               ت
             </p>
           ) : unitDisplayType == "compact" ? (
-            <span className="relative flex h-6 w-6 sm:h-8 sm:w-8">
+            <span
+              className={`relative flex sm:h-8 sm:w-8 ${
+                size === "sm"
+                  ? "h-6 w-6"
+                  : size === "lg"
+                  ? "h-8 w-8"
+                  : "h-8 w-8"
+              }`}
+            >
               <Toman className={`h-full w-full text-${secondaryColor}`}></Toman>
             </span>
           ) : unitDisplayType == "engMinimal" ? (
@@ -96,6 +107,13 @@ export default function PriceTag({
           ) : (
             ""
           )}
+        </div>
+      ) : (
+        <div className={`text-${secondaryColor}`}>
+          <div className="flex h-6 w-20 gap-1">
+            <Skeleton className="h-full w-8/12" />
+            <Skeleton className="h-full w-4/12" />
+          </div>
         </div>
       )}
     </span>
