7adf20c0e437b178246d3e5f3ad9f42ee4968cff
Navid Azimi <navidazimi1380@gmail.com>
2024-10-07 21:52:59 +0330

    refactor(frontend): :recycle: add new variant to cart button

--- frontend/core/src/app/(menu)/[menu_id]/menu/components/CartBtn.tsx
+++ frontend/core/src/app/(menu)/[menu_id]/menu/components/CartBtn.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useRef, useState } from "react";
 
 //libraries
@@ -18,8 +20,13 @@ import useConditionalAnimation from "@/app/hooks/useConditionalAnimation";
 //types
 import { type CategoriesType } from "./Items/MenuItemsWrapper";
 import { type MenuItemType } from "./Items/MenuItem";
+import { ShoppingBag } from "lucide-react";
+import { useTactileAnimation } from "@/app/hooks/useTactileAnimation";
+type CartBtnType = {
+  type?: "default" | "compact";
+};
 
-function CartBtn() {
+function CartBtn({ type = "default" }: CartBtnType) {
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["items"],
     queryFn: async () => {
@@ -37,8 +44,10 @@ function CartBtn() {
 
   const cartBtnRef = useRef<HTMLButtonElement>(null);
 
+  const DrawerIsOpen = useMenuItemDrawer((state) => state.isOpen);
   const cartItems = useItemCart((state) => state.items);
 
+  //store data of items inside cartItems
   const [matchingItems, setMatchingItems] = useState<
     { item: MenuItemType; count: number }[]
   >([]);
@@ -65,8 +74,6 @@ function CartBtn() {
     setMatchingItems(items);
   }, [isLoading, cartItems]);
 
-  const DrawerIsOpen = useMenuItemDrawer((state) => state.isOpen);
-
   //show/hide cart button according to MenuItem Drawer & items count
   useEffect(() => {
     if (DrawerIsOpen) {
@@ -88,49 +95,84 @@ function CartBtn() {
         });
       }
     }
+
+    //automatically animate compact cart button
+    if (type === "compact") {
+      autoTactile();
+    }
   }, [cartItems, DrawerIsOpen]);
 
-  //add animations
-  useConditionalAnimation(cartBtnRef, ["tactile"]);
-
-  return (
-    <Button
-      ref={cartBtnRef}
-      asChild
-      className="!fixed bottom-[-80px] left-0 right-0 z-50 m-3 flex h-max items-center justify-between rounded-full bg-orange-300 p-1.5 shadow-2xl xs:p-2"
-    >
-      <Link href={`/venhan/orders`}>
-        <div className=" flex items-center pr-2">
-          <p className="text-lg font-medium text-orange-900 xs:text-xl xs:font-semibold">
-            ثبت سفارش
-          </p>
-        </div>
-        <section className="flex flex-shrink flex-row-reverse justify-start -space-x-4 ltr:space-x-reverse">
-          {matchingItems.slice(0, 3).map((item) => (
-            <div
-              key={item.item.id}
-              className="relative h-11 w-11 rounded-full border-2 border-orange-300 shadow-sm transition-all duration-300 xs:h-12 xs:w-12"
-            >
-              <Image
-                fill
-                src={"http://127.0.0.1:8000/" + item.item.image}
-                alt={item.item.name}
-                className="rounded-full object-cover"
-              ></Image>
-              <div className="absolute left-0 top-0 z-10 h-4 w-4 rounded-full border border-orange-300 bg-orange-950 pt-0.5 text-center text-xs font-light text-orange-300 transition-all duration-300 xs:h-5 xs:w-5 xs:font-normal">
-                {item.count}
-              </div>
-            </div>
-          ))}
-          {matchingItems.length > 3 && (
-            <div className="h-11 w-11 rounded-full border-2 border-orange-300 bg-orange-950 pt-3 text-center text-orange-200 shadow-sm transition-all duration-300 xs:h-12 xs:w-12">
-              <p>{matchingItems.length - 3}+</p>
-            </div>
-          )}
-        </section>
-      </Link>
-    </Button>
+  //total number of selected items
+  const totalItemCount = cartItems.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.count,
+    0
   );
+
+  //add animations *needs to be checked using props value*
+  useConditionalAnimation(cartBtnRef, ["tactile"]);
+  //automatically add tactile animation (only for compact button)
+  const autoTactile = useTactileAnimation(cartBtnRef, {});
+
+  if (type === "default") {
+    return (
+      <Button
+        ref={cartBtnRef}
+        asChild
+        className="!fixed bottom-[-80px] left-0 right-0 z-50 m-3 flex h-max items-center justify-between rounded-full bg-orange-300 p-2 shadow-2xl xs:p-2.5"
+      >
+        <Link href={`/venhan/orders`}>
+          <div className=" flex items-center pr-2">
+            <p className="text-lg font-medium text-orange-900 xs:text-xl xs:font-semibold">
+              ثبت سفارش
+            </p>
+          </div>
+          <section className="flex flex-shrink flex-row-reverse justify-start -space-x-4 ltr:space-x-reverse">
+            {matchingItems.slice(0, 3).map((item) => (
+              <div
+                key={item.item.id}
+                className="relative h-11 w-11 rounded-full border-2 border-orange-300 shadow-sm transition-all duration-300 xs:h-12 xs:w-12"
+              >
+                <Image
+                  fill
+                  src={"http://127.0.0.1:8000/" + item.item.image}
+                  alt={item.item.name}
+                  className="rounded-full object-cover"
+                ></Image>
+                <div className="absolute -left-0.5 -top-0.5 z-10 h-[18px] w-[18px] rounded-full border border-orange-300 bg-orange-950 pt-0.5 text-center text-xs font-light text-orange-300 transition-all duration-300 xs:h-5 xs:w-5 xs:font-normal">
+                  {item.count}
+                </div>
+              </div>
+            ))}
+            {matchingItems.length > 3 && (
+              <div className="h-11 w-11 rounded-full border-2 border-orange-300 bg-orange-950 pt-3 text-center text-orange-200 shadow-sm transition-all duration-300 xs:h-12 xs:w-12">
+                <p>{matchingItems.length - 3}+</p>
+              </div>
+            )}
+          </section>
+        </Link>
+      </Button>
+    );
+  } else if (type === "compact") {
+    return (
+      <div className="fixed bottom-[-80px] z-50 mb-2 flex w-screen justify-center">
+        <Button
+          ref={cartBtnRef}
+          asChild
+          className="relative h-14 w-14 rounded-full bg-orange-300 shadow-inner"
+          size="icon"
+        >
+          <Link href={`/venhan/orders`}>
+            <ShoppingBag className="h-6 w-6 text-orange-950"></ShoppingBag>
+            {totalItemCount >= 1 && (
+              <div className="absolute -left-0.5 -top-0.5 z-10 h-6 w-6 rounded-full border-2 border-orange-300 bg-orange-950 pt-1 text-center text-xs font-normal text-orange-300 transition-all duration-300">
+                {totalItemCount}
+              </div>
+            )}
+          </Link>
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default CartBtn;
