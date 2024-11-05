02efe9dce7f991857ce51473ca2874d9eded4fde
Navid Azimi <navidazimi1380@gmail.com>
2024-11-05 12:32:02 +0330

    refactor(frontend): :recycle: Change menu route to reflect previous changes
    + add global styling type to menu route + fetch global styling to use in client components and loading skeletons

--- frontend/core/src/app/(menu)/[menu_id]/menu/page.tsx
+++ frontend/core/src/app/(menu)/[menu_id]/menu/page.tsx
@@ -5,16 +5,54 @@ import MenuItemsWrapper from "./components/Items/MenuItemsWrapper";
 import ItemsCategory from "./components/ItemsCategory";
 import CartBtn from "./components/CartBtn";
 import QueryClientWrapper from "./components/QueryClientWrapper";
+import CategoryBtnSkeleton from "./components/categories/CategoryBtnSkeleton";
 
-export default function Page({ params }: { params: { menu_id: string } }) {
+//types
+export type MenuGlobalStyling = {
+  id: number;
+  primary_color: string;
+  secondary_color: string;
+  tertiary_color: string;
+  bg_color: string;
+  unit_display_type: "comp" | "simp" | "engL" | "perL";
+  click_animation: ["tactile"];
+  updated: string;
+  created: string;
+  menu: number;
+};
+
+// GET menu global stylings
+export async function getGlobalStyling(menu_id: string) {
+  try {
+    let res = await fetch(
+      `http://127.0.0.1:8000/menu/${menu_id}/global-styling/`
+    );
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("error fetching data:", error);
+  }
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { menu_id: string };
+}) {
+  const globalStyling = await getGlobalStyling(params.menu_id);
+
   return (
     <div className="container relative flex flex-col gap-2 p-0">
-      <Suspense fallback="loading categories...">
+      <Suspense
+        fallback={<CategoryBtnSkeleton globalStyling={globalStyling} />}
+      >
         <ItemsCategory params={params}></ItemsCategory>
       </Suspense>
       <QueryClientWrapper>
         <MenuItemsWrapper params={params}></MenuItemsWrapper>
-        <CartBtn type="compact"></CartBtn>
+        <CartBtn type="compact" globalStyling={globalStyling}></CartBtn>
       </QueryClientWrapper>
     </div>
   );
