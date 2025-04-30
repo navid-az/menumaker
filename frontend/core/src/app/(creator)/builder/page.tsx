6270158d78c6a9eb15cd9de67f89bf356b345afc
Navid Azimi <navidazimi1380@gmail.com>
2025-04-30 15:58:41 +0330

    refactor(frontend): :recycle: fetch and pass assetGroups data to BuilderForm in server component

--- frontend/core/src/app/(creator)/builder/page.tsx
+++ frontend/core/src/app/(creator)/builder/page.tsx
@@ -1,10 +1,22 @@
 //components
 import MenuMaker from "./components/MenuMaker";
 
+const getAssetGroups = async () => {
+  const res = await fetch("http://127.0.0.1:8000/pickers/icon-pickers");
+  if (!res.ok) {
+    throw new Error(
+      `Failed to fetch asset groups: ${res.status} ${res.statusText}`
+    );
+  }
+  return await res.json();
+};
+
 export default async function Page() {
+  const assetGroups = await getAssetGroups();
+
   return (
     <section className="container m-auto flex h-screen w-full items-center justify-between gap-12 overflow-hidden">
-      <MenuMaker></MenuMaker>
+      <MenuMaker assetGroups={assetGroups}></MenuMaker>
     </section>
   );
 }
