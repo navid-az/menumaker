3805810be9c92b59fd37e0ef3082661052868f16
Navid Azimi <navidazimi1380@gmail.com>
2025-05-01 20:20:27 +0330

    feat(api): :sparkles: create menu section model

--- backend/core/menu/admin.py
+++ backend/core/menu/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Menu, MenuGlobalStyling, Table, Tag
+from .models import Menu, MenuSection, MenuGlobalStyling, Table, Tag
 
 admin.site.register(Table)
 admin.site.register(Tag)
+admin.site.register(MenuSection)
 
 
 class MenuGlobalStylingInline(admin.StackedInline):
--- backend/core/menu/models.py
+++ backend/core/menu/models.py
@@ -35,6 +35,17 @@ class Menu(models.Model):
         return f"[{self.business.name_en if self.business else 'Unknown Business!'}]'s menu"
 
 
+class MenuSection(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, blank=True, null=True)
+    icon = models.ForeignKey('pickers.Asset', on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.menu if self.menu else 'Unknown Menu!'} section[{self.name}]"
+
+
 # stylings which effect the entire menu
 class MenuGlobalStyling(models.Model):
     CLICK_ANIMATION_CHOICES = [
