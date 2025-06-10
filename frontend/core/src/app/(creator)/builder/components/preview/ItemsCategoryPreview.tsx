1dae1cad4fd0fb69efa987fd2362774acc07a986
Navid Azimi <navidazimi1380@gmail.com>
2025-06-10 13:05:08 +0330

    fix(frontend): :bug: fix dummy category buttons not rendering their icons
    + shortened categories mockup data and only included necessary data

--- frontend/core/src/app/(creator)/builder/components/preview/ItemsCategoryPreview.tsx
+++ frontend/core/src/app/(creator)/builder/components/preview/ItemsCategoryPreview.tsx
@@ -3,150 +3,6 @@
 //components
 import CategoryBtnPreview from "./CategoryBtnPreview";
 
-// mockup data
-const categoryData = [
-  {
-    id: 1,
-    menu: "venhan",
-    items: [
-      {
-        id: 63,
-        menu: "venhan",
-        category: "سالاد",
-        name: "سالاد سزار",
-        description:
-          "کاهو,سس سزار,زیتون سیاه,نان مخصوص, روغن زیتون,۲۵۰ گرم سینه مرغ گریل شده,گوجه تازه",
-        image: "/media/menu/items/images/salad-sezar-680607864_mMwX2iP.jpg",
-        price: 267000,
-        is_available: true,
-        is_active: true,
-      },
-      {
-        id: 64,
-        menu: "venhan",
-        category: "سالاد",
-        name: "سالاد مخصو وایل وود",
-        description: "",
-        image: "/media/menu/items/images/50-1504337676_rsdSWwT.jpg",
-        price: 324000,
-        is_available: false,
-        is_active: false,
-      },
-      {
-        id: 65,
-        menu: "venhan",
-        category: "سالاد",
-        name: "مون سالاد",
-        description: "",
-        image:
-          "/media/menu/items/images/%D9%85%D9%88%D9%86-%D8%B3%D8%A7%D9%84%D8%A7%D8%AF-1657896697_ZNDjYeI.jpg",
-        price: 214000,
-        is_available: false,
-        is_active: false,
-      },
-    ],
-    icon: {
-      name: "salad",
-      image: "/media/iconPicker/icons/salad-colored-outline.svg",
-    },
-    name: "سالاد",
-    text_color: "#FFFFFF",
-    child_bg: "#FFFFFF",
-    parent_bg: "#FFFFFF",
-    is_active: true,
-  },
-  {
-    id: 2,
-    menu: "venhan",
-    items: [
-      {
-        id: 55,
-        menu: "venhan",
-        category: "برگر",
-        name: "برگر قارچ",
-        description: "۲۰۰ گرم گوشت راسته",
-        image: "/media/menu/items/images/Double-Cheeseburger-1_zxnT1WT.jpg",
-        price: 250000,
-        is_available: false,
-        is_active: true,
-      },
-      {
-        id: 61,
-        menu: "venhan",
-        category: "برگر",
-        name: "سیب زمینی",
-        description: "",
-        image: "/media/menu/items/images/fires.jpg",
-        price: 20000,
-        is_available: false,
-        is_active: false,
-      },
-    ],
-    icon: {
-      name: "burger",
-      image: "/media/iconPicker/icons/cheeseburger-colored-outline.svg",
-    },
-    name: "برگر",
-    text_color: "#FFFFFF",
-    child_bg: "#FFFFFF",
-    parent_bg: "#FFFFFF",
-    is_active: true,
-  },
-  {
-    id: 9,
-    menu: "venhan",
-    items: [
-      {
-        id: 66,
-        menu: "venhan",
-        category: "پاستا",
-        name: "پاستا آلفردو",
-        description: "خیلی خوبه قول میدم",
-        image: "/media/menu/items/images/unnamed-768x596-3631302702.jpg",
-        price: 328000,
-        is_available: true,
-        is_active: false,
-      },
-    ],
-    icon: {
-      name: "spaghetti",
-      image: "/media/iconPicker/icons/pasta-minimal.svg",
-    },
-    name: "پاستا",
-    text_color: "#FFFFFF",
-    child_bg: "#FFFFFF",
-    parent_bg: "#FFFFFF",
-    is_active: true,
-  },
-  {
-    id: 11,
-    menu: "venhan",
-    items: [
-      {
-        id: 67,
-        menu: "venhan",
-        category: "نوشیدنی های سرد",
-        name: "آفوگاتو",
-        description: "",
-        image:
-          "/media/menu/items/images/%D8%B7%D8%B1%D8%B2-%D8%AA%D9%87%DB%8C%D9%87-%D8%A2%D9%81%D9%88%DA%AF%D8%A7%D8%AA%D9%88-2422785558.jpg",
-        price: 93000,
-        is_available: true,
-        is_active: true,
-      },
-    ],
-    icon: {
-      name: "lemon-juice",
-      image: "/media/iconPicker/icons/lemonjuice-colored-outline.svg",
-    },
-    name: "نوشیدنی های سرد",
-    text_color: "#FFFFFF",
-    child_bg: "#FFFFFF",
-    parent_bg: "#FFFFFF",
-    is_active: true,
-  },
-];
-
 export type CategoryType = {
   id: number;
   name: string;
@@ -173,6 +29,13 @@ export default function ItemsCategoryPreview({
   colors,
   globalBorderRadius,
 }: ItemsCategoryType) {
+  // mockup data
+  const categoryData = [
+    { name: "پیتزا", icon: "/media/iconPicker/icons/pizza_colored.svg" },
+    { name: "برگر", icon: "/media/iconPicker/icons/burger.svg" },
+    { name: "نوشیدنی های سرد", icon: "/media/iconPicker/icons/drink.svg" },
+    { name: "نوشیدنی های گرم", icon: "/media/iconPicker/icons/tea.svg" },
+  ];
   return (
     <div
       className={`hide-scrollbar avoid-stretch h-200px overflow-x-auto pb-4 backdrop-blur-lg ${
@@ -183,13 +46,13 @@ export default function ItemsCategoryPreview({
           : "flex-color h-full w-2/12 flex-col gap-4"
       }`}
     >
-      {categoryData.map((category) => (
+      {categoryData.map((category, index) => (
         <CategoryBtnPreview
-          key={category.id}
-          id={category.id.toString()}
+          key={index}
+          id={index.toString()}
           name={category.name}
           parentType={type}
-          icon={category.icon.image}
+          icon={category.icon}
           animations={["ripple", "tactile"]}
           colors={colors}
           globalBorderRadius={globalBorderRadius}
