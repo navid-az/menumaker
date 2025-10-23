4f65c16e911f1b2320b1410c4d9e41172c7637bc
Navid Azimi <navidazimi1380@gmail.com>
2025-10-23 18:08:33 +0330

    feat(frontend): :sparkles: initial live management introduction page hero design
    - Designed initial live management introdcution hero section - Added more custom duo tone svgs

--- frontend/core/src/app/(landing)/liveManagement/page.tsx
+++ frontend/core/src/app/(landing)/liveManagement/page.tsx
@@ -11,6 +11,10 @@ import {
   Infinity,
   CloudCheck,
   ClockAlert,
+  Radar,
+  ScrollText,
+  Settings,
+  Chart,
 } from "@/app/components/icons/DuoTone";
 
 export const liveDummyTables: TableType[] = [
@@ -195,32 +199,159 @@ export const liveDummyTables: TableType[] = [
 
 export default function Page() {
   return (
-    <div className="relative lg:flex-row flex-col gap-8 w-full flex h-[2000px] overflow-hidden px-4 lg:px-12 xl:px-16">
-      <div className="flex flex-col gap-8 w-11/12 lg:basis-2/5">
-        <h2 className="text-royal-green md:text-5xl/18 sm:text-4xl/14 xl:text-5xl/18 text-xl/8">
-          مشاهده وضعیت میز ها
-          <br className="hidden md:flex" /> و سفارشات آنلاین به{" "}
-          <br className="hidden md:flex" />
-          صورت{" "}
-          <span className="text-grad bg-gradient-to-b from-[#94D9E2] to-[#06434C] bg-clip-text text-transparent">
-            لحظه ای
-          </span>
-        </h2>
-        <div className="flex flex-col gap-4">
-          <h4 className="text-xs sm:text-base text-royal-green/70">
-            <span className="text-royal-green font-semibold">
-              به سادگی از وضعیت هر سفارش با خبر شوید.
-            </span>{" "}
-            هر کارت، نمای زنده‌ای از وضعیت سالن شماست. درخواست‌ها، سفارش‌ها و
-            وضعیت میزها — همگی در یک نگاه.
-          </h4>
-          <h5 className="text-xs lg:text-base">
-            از درخواست صورت حساب و اطلاع از سفارش مشتری تا وضعیت پرداخت و زمان
-            رزرو میز
-          </h5>
+    <div className="h-full w-full">
+      <section className="relative overflow-hidden flex flex-col justify-center items-center mb-[2000px]">
+        <div className="relative flex flex-col justify-between my-16 sm:my-24 md:my-32 lg:my-42 w-full">
+          {/* icons */}
+          <div className="flex justify-between self-end px-4 w-full lg:px-24">
+            <Settings
+              primaryColor="var(--royal-green)"
+              strokeWidth={1.5}
+              className="w-5 h-5 sm:w-8 sm:h-8 opacity-35 md:w-9 md:h-9 lg:w-14 lg:h-14"
+            ></Settings>
+            <ScrollText
+              primaryColor="var(--royal-green)"
+              strokeWidth={1.5}
+              className="w-8 h-8 sm:w-12 sm:h-12 opacity-55 md:w-14 md:h-14 lg:w-22 lg:h-22 mt-5"
+            ></ScrollText>
+            <Radar
+              primaryColor="var(--royal-green)"
+              strokeWidth={1.5}
+              className="w-15 h-15 sm:w-22 sm:h-22 md:w-28 md:h-28 lg:w-42 lg:h-42 mt-10"
+            ></Radar>
+            <Chart
+              primaryColor="var(--royal-green)"
+              strokeWidth={1.5}
+              className="w-8 h-8 sm:w-12 sm:h-12 opacity-55 md:w-14 md:h-14 lg:w-22 lg:h-22 mt-5"
+            ></Chart>
+            <Users
+              primaryColor="var(--royal-green)"
+              strokeWidth={1.5}
+              className="w-5 h-5 sm:w-8 sm:h-8 opacity-35 md:w-9 md:h-9 lg:w-14 lg:h-14"
+            ></Users>
+          </div>
+          {/* curved line */}
+          <svg
+            className="absolute -bottom-6"
+            width="100%"
+            viewBox="0 0 1441 148"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.203156 0.454102C0.203156 0.454102 323.203 146.954 720.203 146.954C1117.2 146.954 1440.2 0.454102 1440.2 0.454102"
+              stroke="var(--royal-green)"
+              strokeWidth={2}
+              strokeOpacity="0.1"
+            />
+          </svg>
         </div>
-        <div className="gap-10 w-full hidden sm:flex">
+        {/* blue glow  */}
+        <div className="absolute bottom-0 z-5 w-8/12 h-1/4 bg-[#4EEBFF]/40 rounded-full blur-[120px]"></div>
+        {/* hero title  */}
+        <h1 className="text-center sm:text-xl md:text-3xl lg:text-4xl/14">
+          مدیریت مجموعه در هر لحظه و هر زمان <br />
+          به صورت{" "}
+          <span className="text-grad bg-gradient-to-b from-[#94D9E2] to-[#06434C] bg-clip-text text-transparent">
+            زنده
+          </span>
+        </h1>
+        {/* arc  */}
+        <svg
+          className="z-20"
+          width="100%"
+          viewBox="0 0 1441 319"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M720.5 147.276C323.5 147.276 0.5 0.775879 0.5 0.775879V317.776H1440.5V0.775879C1440.5 0.775879 1117.5 147.276 720.5 147.276Z"
+            fill="white"
+            stroke="#0F2C30"
+            strokeOpacity="0.1"
+          />
+        </svg>
+      </section>
+
+      {/* dashboard mockup */}
+      <div className="relative lg:flex-row flex-col gap-8 w-full flex h-[2000px] overflow-hidden px-4 lg:px-12 xl:px-16">
+        <div className="flex flex-col gap-8 w-11/12 lg:basis-2/5">
+          <h2 className="text-royal-green md:text-5xl/18 sm:text-4xl/14 xl:text-5xl/18 text-xl/8">
+            مشاهده وضعیت میز ها
+            <br className="hidden md:flex" /> و سفارشات آنلاین به{" "}
+            <br className="hidden md:flex" />
+            صورت{" "}
+            <span className="text-grad bg-gradient-to-b from-[#94D9E2] to-[#06434C] bg-clip-text text-transparent">
+              لحظه ای
+            </span>
+          </h2>
           <div className="flex flex-col gap-4">
+            <h4 className="text-xs sm:text-base text-royal-green/70">
+              <span className="text-royal-green font-semibold">
+                به سادگی از وضعیت هر سفارش با خبر شوید.
+              </span>{" "}
+              هر کارت، نمای زنده‌ای از وضعیت سالن شماست. درخواست‌ها، سفارش‌ها و
+              وضعیت میزها — همگی در یک نگاه.
+            </h4>
+            <h5 className="text-xs lg:text-base">
+              از درخواست صورت حساب و اطلاع از سفارش مشتری تا وضعیت پرداخت و زمان
+              رزرو میز
+            </h5>
+          </div>
+          <div className="gap-10 w-full hidden sm:flex">
+            <div className="flex flex-col gap-4">
+              <FeatureHighlight
+                icon={
+                  <CloudCheck
+                    className="w-4 h-4 lg:w-5 lg:h-5"
+                    primaryColor="var(--royal-green)"
+                  ></CloudCheck>
+                }
+                text="ذخیره سفارشات"
+              ></FeatureHighlight>
+              <FeatureHighlight
+                icon={
+                  <Users
+                    className="w-4 h-4 lg:w-5 lg:h-5"
+                    primaryColor="var(--royal-green)"
+                    size={20}
+                  ></Users>
+                }
+                text="فیلتر اطلاعات بر اساس سمت"
+              ></FeatureHighlight>
+            </div>
+            <div className="flex flex-col gap-4">
+              <FeatureHighlight
+                icon={
+                  <Infinity
+                    className="w-4 h-4 lg:w-5 lg:h-5"
+                    primaryColor="var(--royal-green)"
+                    size={20}
+                  ></Infinity>
+                }
+                text="ثبت بی نهایت میز"
+              ></FeatureHighlight>
+              <FeatureHighlight
+                icon={
+                  <ClockAlert
+                    className="w-4 h-4 lg:w-5 lg:h-5"
+                    primaryColor="var(--royal-green)"
+                    size={20}
+                  ></ClockAlert>
+                }
+                text="نمایش زمان و تاریخ اعلان"
+              ></FeatureHighlight>
+            </div>
+          </div>
+        </div>
+        <div className="relative lg:absolute xl:-left-[500px] h-max 2xl:-left-[450px] lg:-left-[700px] max-h-[280px]">
+          <div className="relative w-max scale-45 sm:scale-85 lg:scale-75 xl:scale-85 2xl:scale-100 origin-top-right pr-32 sm:pr-48 lg:pr-0">
+            <LiveManagementMockUp></LiveManagementMockUp>
+            <div className="absolute z-50 w-full -bottom-2 left-0 h-[350px] bg-gradient-to-t from-white from-45% via-white/95 via-55% to-transparent pointer-events-none" />
+          </div>
+        </div>
+        <div className="sm:gap-10 sm:hidden sm:justify-center justify-between w-full z-30 flex mt-8">
+          <div className="flex flex-col gap-2">
             <FeatureHighlight
               icon={
                 <CloudCheck
@@ -241,7 +372,7 @@ export default function Page() {
               text="فیلتر اطلاعات بر اساس سمت"
             ></FeatureHighlight>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
             <FeatureHighlight
               icon={
                 <Infinity
@@ -263,57 +394,6 @@ export default function Page() {
               text="نمایش زمان و تاریخ اعلان"
             ></FeatureHighlight>
           </div>
-        </div>
-      </div>
-      <div className="relative lg:absolute xl:-left-[500px] h-max 2xl:-left-[450px] lg:-left-[700px] max-h-[280px]">
-        <div className="relative w-max scale-45 sm:scale-85 lg:scale-75 xl:scale-85 2xl:scale-100 origin-top-right pr-32 sm:pr-48 lg:pr-0">
-          <LiveManagementMockUp></LiveManagementMockUp>
-          <div className="absolute z-50 w-full -bottom-2 left-0 h-[350px] bg-gradient-to-t from-white from-45% via-white/95 via-55% to-transparent pointer-events-none" />
-        </div>
-      </div>
-      <div className="sm:gap-10 sm:hidden sm:justify-center justify-between w-full z-30 flex mt-8">
-        <div className="flex flex-col gap-2">
-          <FeatureHighlight
-            icon={
-              <CloudCheck
-                className="w-4 h-4 lg:w-5 lg:h-5"
-                primaryColor="var(--royal-green)"
-              ></CloudCheck>
-            }
-            text="ذخیره سفارشات"
-          ></FeatureHighlight>
-          <FeatureHighlight
-            icon={
-              <Users
-                className="w-4 h-4 lg:w-5 lg:h-5"
-                primaryColor="var(--royal-green)"
-                size={20}
-              ></Users>
-            }
-            text="فیلتر اطلاعات بر اساس سمت"
-          ></FeatureHighlight>
-        </div>
-        <div className="flex flex-col gap-2">
-          <FeatureHighlight
-            icon={
-              <Infinity
-                className="w-4 h-4 lg:w-5 lg:h-5"
-                primaryColor="var(--royal-green)"
-                size={20}
-              ></Infinity>
-            }
-            text="ثبت بی نهایت میز"
-          ></FeatureHighlight>
-          <FeatureHighlight
-            icon={
-              <ClockAlert
-                className="w-4 h-4 lg:w-5 lg:h-5"
-                primaryColor="var(--royal-green)"
-                size={20}
-              ></ClockAlert>
-            }
-            text="نمایش زمان و تاریخ اعلان"
-          ></FeatureHighlight>
         </div>
       </div>
     </div>
@@ -355,7 +435,7 @@ const LiveManagementMockUp = () => {
         <div className="w-full gap-4 flex">
           {liveDummyTables.slice(0, 5).map((table, index) => (
             <LiveCard
-            isMockUp
+              isMockUp
               key={index}
               table={table}
               businessSlug="navid"
@@ -366,7 +446,7 @@ const LiveManagementMockUp = () => {
         <div className="w-full gap-4 flex">
           {liveDummyTables.slice(5).map((table, index) => (
             <LiveCard
-            isMockUp
+              isMockUp
               key={index}
               table={table}
               businessSlug="navid"
--- frontend/core/src/app/components/icons/DuoTone.tsx
+++ frontend/core/src/app/components/icons/DuoTone.tsx
@@ -142,7 +142,7 @@ export function Users({
       <path
         d="M16 3.13C16.8604 3.3503 17.623 3.8507 18.1676 4.55231C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89317 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88"
         stroke={secondaryColor}
-        strokeWidth="2"
+        strokeWidth={strokeWidth}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -181,6 +181,214 @@ export function Infinity({
       <path
         d="M10.0001 10.0001C9.16675 10.4167 8.75008 12.9168 5.00008 13.3334C4.11603 13.3334 3.26818 12.9821 2.64306 12.357C2.01794 11.7319 1.66675 10.8841 1.66675 10C1.66675 9.11594 2.01794 8.2681 2.64306 7.64298C3.26818 7.01786 4.12144 6.56902 5.00008 6.66667C7.76572 6.97401 7.77796 7.77794 10.0001 10.0001Z"
         stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+export function Radar({
+  size = 24,
+  primaryColor = "#C5E5E9",
+  secondaryColor = "#94D9E2",
+  strokeWidth = 2,
+  className,
+}: {
+  size?: number;
+  primaryColor?: string;
+  secondaryColor?: string;
+  strokeWidth?: number;
+  className?: string;
+}) {
+  return (
+    <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.9001 19.1C1.0001 15.2 1.0001 8.8 4.9001 4.9"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.79995 16.2C5.49995 13.9 5.49995 10.1 7.79995 7.7"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16.2 7.8C18.5 10.1 18.5 13.9 16.2 16.3"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.1001 4.9C23.0001 8.8 23.0001 15.1 19.1001 19"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function ScrollText({
+  size = 24,
+  primaryColor = "#C5E5E9",
+  secondaryColor = "#94D9E2",
+  strokeWidth = 2,
+  className,
+}: {
+  size?: number;
+  primaryColor?: string;
+  secondaryColor?: string;
+  strokeWidth?: number;
+  className?: string;
+}) {
+  return (
+    <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 21H20C20.5304 21 21.0391 20.7893 21.4142 20.4142C21.7893 20.0391 22 19.5304 22 19V17H10V19C10 19.5304 9.78929 20.0391 9.41421 20.4142C9.03914 20.7893 8.53043 21 8 21ZM8 21C7.46957 21 6.96086 20.7893 6.58579 20.4142C6.21071 20.0391 6 19.5304 6 19V5C6 4.46957 5.78929 3.96086 5.41421 3.58579C5.03914 3.21071 4.53043 3 4 3C3.46957 3 2.96086 3.21071 2.58579 3.58579C2.21071 3.96086 2 4.46957 2 5V8H6"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19 17V5C19 4.46957 18.7893 3.96086 18.4142 3.58579C18.0391 3.21071 17.5304 3 17 3H4"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 8H10"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 12H10"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Chart({
+  size = 24,
+  primaryColor = "#C5E5E9",
+  secondaryColor = "#94D9E2",
+  strokeWidth = 2,
+  className,
+}: {
+  size?: number;
+  primaryColor?: string;
+  secondaryColor?: string;
+  strokeWidth?: number;
+  className?: string;
+}) {
+  return (
+    <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 3V21H21"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18 17V9"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13 17V5"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 17V14"
+        stroke={secondaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Settings({
+  size = 24,
+  primaryColor = "#C5E5E9",
+  secondaryColor = "#94D9E2",
+  strokeWidth = 2,
+  className,
+}: {
+  size?: number;
+  primaryColor?: string;
+  secondaryColor?: string;
+  strokeWidth?: number;
+  className?: string;
+}) {
+  return (
+    <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.22 2H11.78C11.2496 2 10.7409 2.21071 10.3658 2.58579C9.99072 2.96086 9.78 3.46957 9.78 4V4.18C9.77964 4.53073 9.68706 4.87519 9.51154 5.17884C9.33602 5.48248 9.08374 5.73464 8.78 5.91L8.35 6.16C8.04596 6.33554 7.70108 6.42795 7.35 6.42795C6.99893 6.42795 6.65404 6.33554 6.35 6.16L6.2 6.08C5.74107 5.81526 5.19584 5.74344 4.684 5.88031C4.17217 6.01717 3.73555 6.35154 3.47 6.81L3.25 7.19C2.98526 7.64893 2.91345 8.19416 3.05031 8.706C3.18717 9.21783 3.52154 9.65445 3.98 9.92L4.13 10.02C4.43228 10.1945 4.68362 10.4451 4.85905 10.7468C5.03448 11.0486 5.1279 11.391 5.13 11.74V12.25C5.1314 12.6024 5.03965 12.949 4.86405 13.2545C4.68844 13.5601 4.43521 13.8138 4.13 13.99L3.98 14.08C3.52154 14.3456 3.18717 14.7822 3.05031 15.294C2.91345 15.8058 2.98526 16.3511 3.25 16.81L3.47 17.19C3.73555 17.6485 4.17217 17.9828 4.684 18.1197C5.19584 18.2566 5.74107 18.1847 6.2 17.92L6.35 17.84C6.65404 17.6645 6.99893 17.5721 7.35 17.5721C7.70108 17.5721 8.04596 17.6645 8.35 17.84L8.78 18.09C9.08374 18.2654 9.33602 18.5175 9.51154 18.8212C9.68706 19.1248 9.77964 19.4693 9.78 19.82V20C9.78 20.5304 9.99072 21.0391 10.3658 21.4142C10.7409 21.7893 11.2496 22 11.78 22H12.22C12.7504 22 13.2591 21.7893 13.6342 21.4142C14.0093 21.0391 14.22 20.5304 14.22 20V19.82C14.2204 19.4693 14.3129 19.1248 14.4885 18.8212C14.664 18.5175 14.9163 18.2654 15.22 18.09L15.65 17.84C15.954 17.6645 16.2989 17.5721 16.65 17.5721C17.0011 17.5721 17.346 17.6645 17.65 17.84L17.8 17.92C18.2589 18.1847 18.8042 18.2566 19.316 18.1197C19.8278 17.9828 20.2645 17.6485 20.53 17.19L20.75 16.8C21.0147 16.3411 21.0866 15.7958 20.9497 15.284C20.8128 14.7722 20.4785 14.3356 20.02 14.07L19.87 13.99C19.5648 13.8138 19.3116 13.5601 19.136 13.2545C18.9604 12.949 18.8686 12.6024 18.87 12.25V11.75C18.8686 11.3976 18.9604 11.051 19.136 10.7455C19.3116 10.4399 19.5648 10.1862 19.87 10.01L20.02 9.92C20.4785 9.65445 20.8128 9.21783 20.9497 8.706C21.0866 8.19416 21.0147 7.64893 20.75 7.19L20.53 6.81C20.2645 6.35154 19.8278 6.01717 19.316 5.88031C18.8042 5.74344 18.2589 5.81526 17.8 6.08L17.65 6.16C17.346 6.33554 17.0011 6.42795 16.65 6.42795C16.2989 6.42795 15.954 6.33554 15.65 6.16L15.22 5.91C14.9163 5.73464 14.664 5.48248 14.4885 5.17884C14.3129 4.87519 14.2204 4.53073 14.22 4.18V4C14.22 3.46957 14.0093 2.96086 13.6342 2.58579C13.2591 2.21071 12.7504 2 12.22 2Z"
+        stroke={primaryColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"
+        stroke={secondaryColor}
         strokeWidth={strokeWidth}
         strokeLinecap="round"
         strokeLinejoin="round"
